2bdda861c2656742fb476f7d303e94036361bec1
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-20 20:42:49 +0000

    Adding animation and ScrollMagic indicators

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+// * Init ScrollMagic
 const controller = new ScrollMagic.Controller();
 
+// * Create Scene
 const ourScene = new ScrollMagic.Scene({
   triggerElement: '#project01',
 })
   .setClassToggle('#project01', 'fade-in')
+  .addIndicators({
+    name: 'fade Scene',
+    colorTrigger: 'black',
+    indent: 200,
+    colorStart: '#79c695',
+  }) // Requires a plugin
   .addTo(controller);
