aa7be799665752faa77decf88d24de4344540d1a
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-21 20:32:42 +0000

    Multiple ScrollMagic items the right way

--- js/main.js
+++ js/main.js
@@ -1,50 +1,25 @@
 // * Init ScrollMagic
 const controller = new ScrollMagic.Controller();
 
-// * Create Scene
-const ourScene = new ScrollMagic.Scene({
-  triggerElement: '#project01 img',
-  // duration: '90%',
-  triggerHook: 0.8,
-  reverse: false,
-})
-  .setClassToggle('#project01', 'fade-in')
-  .addIndicators({
-    name: 'fade scene 1',
-    colorTrigger: 'black',
-    colorStart: '#79c695',
-    colorEnd: 'red',
-  }) // Requires a plugin
-  .addTo(controller);
+// * Collect elements to fade in
+const projects = document.querySelectorAll('.project');
+let counter = 1;
 
-// * Create Scene
-const ourScene2 = new ScrollMagic.Scene({
-  triggerElement: '#project02 img',
-  // duration: '90%',
-  triggerHook: 0.8,
-  reverse: false,
-})
-  .setClassToggle('#project02', 'fade-in')
-  .addIndicators({
-    name: 'fade scene 2',
-    colorTrigger: 'black',
-    colorStart: '#79c695',
-    colorEnd: 'red',
-  }) // Requires a plugin
-  .addTo(controller);
-
-// * Create Scene
-const ourScene3 = new ScrollMagic.Scene({
-  triggerElement: '#project03 img',
-  // duration: '90%',
-  triggerHook: 0.8,
-  reverse: false,
-})
-  .setClassToggle('#project03', 'fade-in')
-  .addIndicators({
-    name: 'fade scene 3',
-    colorTrigger: 'black',
-    colorStart: '#79c695',
-    colorEnd: 'red',
-  }) // Requires a plugin
-  .addTo(controller);
+projects.forEach(function(item) {
+  // * Create Scene
+  const ourScene = new ScrollMagic.Scene({
+    triggerElement: item.children[0],
+    // duration: '90%',
+    triggerHook: 0.8,
+    // reverse: false,
+  })
+    .setClassToggle(item, 'fade-in')
+    .addIndicators({
+      name: `fade scene ${counter}`,
+      colorTrigger: 'black',
+      colorStart: '#79c695',
+      colorEnd: 'red',
+    }) // Requires a plugin
+    .addTo(controller);
+  counter++;
+});
