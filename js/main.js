6c3fe06784f23624531e8ec8878e3a60900200ab
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-21 09:00:18 +0000

    Adding duration and moving trigger hook

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ const controller = new ScrollMagic.Controller();
 
 // * Create Scene
 const ourScene = new ScrollMagic.Scene({
-  triggerElement: '#project01',
+  triggerElement: '#project01 img',
+  duration: '90%',
+  triggerHook: 0.9,
 })
   .setClassToggle('#project01', 'fade-in')
   .addIndicators({
     name: 'fade Scene',
     colorTrigger: 'black',
-    indent: 200,
     colorStart: '#79c695',
+    colorEnd: 'red',
   }) // Requires a plugin
   .addTo(controller);
