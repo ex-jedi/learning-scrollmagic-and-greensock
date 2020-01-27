8be48cee8cf726a039454a69722f75d5bccee2d1
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-27 19:17:45 +0000

    Adding parallax section - half done

--- js/main.js
+++ js/main.js
@@ -29,6 +29,21 @@ let pinIntroSceneTwo = new ScrollMagic.Scene({
     colorEnd: 'red',
   }); // Requires a plugin;;
 
+// * Parallax Scene
+let slideParallaxScene = new ScrollMagic.Scene({
+  triggerElement: '.bcg-parallax',
+  triggerHook: 1,
+  duration: '200%',
+})
+  .setTween(TweenMax.from('.bcg', 1, { y: '-30%', ease: Power0.easeNone }))
+  .addTo(controller)
+  .addIndicators({
+    name: 'parallax',
+    colorTrigger: 'blue',
+    colorStart: 'blue',
+    colorEnd: 'red',
+  }); // Requires a plugin;;
+
 // * Collect elements to fade in
 const projects = document.querySelectorAll('.project');
 let counter = 1;
