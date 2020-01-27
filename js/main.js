b7659a0631cd05373cebfe7974fb217e5ccad690
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-01-27 19:28:00 +0000

    Parallax effect mostly done

--- js/main.js
+++ js/main.js
@@ -33,9 +33,9 @@ let pinIntroSceneTwo = new ScrollMagic.Scene({
 let slideParallaxScene = new ScrollMagic.Scene({
   triggerElement: '.bcg-parallax',
   triggerHook: 1,
-  duration: '200%',
+  duration: '150%',
 })
-  .setTween(TweenMax.from('.bcg', 1, { y: '-30%', ease: Power0.easeNone }))
+  .setTween(TweenMax.from('.bcg', 0.5, { y: '-50%', ease: Power0.easeNone }))
   .addTo(controller)
   .addIndicators({
     name: 'parallax',
